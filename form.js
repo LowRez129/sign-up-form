ae06fe8d4cb1b31f02e5dfce352af4ea333c951b
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-08-07 09:02:15 +1200

    Edit html js

--- form.js
+++ form.js
@@ -1,7 +1,23 @@
 const password = document.querySelector("#password");
+const confirm_password = document.querySelector("#confirm-password");
 const first_name = document.querySelector("#first-name");
+const email = document.querySelector("#email");
 const submit_button = document.querySelector("#submit-button");
 
 submit_button.addEventListener("click", () => {
-    console.log(first_name.value);
+    console.log(password.value, confirm_password.value);
+
+    switch (password.value) {
+        case "":
+            console.log("Empty");
+            break;
+        
+        case confirm_password.value:
+            console.log("Ok");
+            break;
+        
+        default:
+            console.log("Error");
+    }
+    
 });
