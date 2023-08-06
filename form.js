fc290bc107954194ab5b637ddd48c3f1a2b889be
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-08-06 15:02:15 +1200

    Edit html css

--- form.js
+++ form.js
@@ -2,4 +2,6 @@ const password = document.querySelector("#password");
 const first_name = document.querySelector("#first-name");
 const submit_button = document.querySelector("#submit-button");
 
-
+submit_button.addEventListener("click", () => {
+    console.log(first_name.value);
+});
